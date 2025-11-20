e40e246746e73949d122c91f6f1146ec64b0bb79
ilyassbz69 <ilyassbenzineb@gmail.com>
2025-11-20 23:43:52 +0100

    connecte to server rmi and retrive the object

--- backend/parking_rmi/src/main/java/com/example/parking_rmi/ParkingRmiApplication.java
+++ backend/parking_rmi/src/main/java/com/example/parking_rmi/ParkingRmiApplication.java
@@ -1,9 +1,13 @@
 package com.example.parking_rmi;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.example.parking_rmi.Interface.ParkingService;
 
 @SpringBootApplication
 public class ParkingRmiApplication {
@@ -11,6 +15,5 @@ public class ParkingRmiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingRmiApplication.class, args);
 	}
-
 
 }
---
+++ backend/parking_rmi/src/main/java/com/example/parking_rmi/service/ParkingServ.java
@@ -0,0 +1,15 @@
+package com.example.parking_rmi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.parking_rmi.Interface.ParkingService;
+
+@Service
+public class ParkingServ {
+    @Autowired
+    ParkingService parkingService;
+
+
+    
+}

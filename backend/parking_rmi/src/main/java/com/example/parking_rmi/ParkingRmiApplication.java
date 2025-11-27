947692dc351277426b6ec97a2891c626f3db4909
ilyassbz69 <ilyassbenzineb@gmail.com>
2025-11-27 22:04:01 +0100

    ajoute les log et variable global

--- backend/parking_rmi/src/main/java/com/example/parking_rmi/ParkingRmiApplication.java
+++ backend/parking_rmi/src/main/java/com/example/parking_rmi/ParkingRmiApplication.java
@@ -1,13 +1,9 @@
 package com.example.parking_rmi;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import com.example.parking_rmi.Interface.ParkingService;
 
 @SpringBootApplication
 public class ParkingRmiApplication {
